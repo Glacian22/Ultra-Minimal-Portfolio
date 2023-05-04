1f59c3d5e0a9a9dd58b057481132ffbdf237eb74
Abram Thau <glacian22@gmail.com>
2023-05-04 01:40:59 -0700

    actually disabled contact

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Nav from './components/nav/nav'
 import NavComp from './components/navcomp/navcomp'
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
 import About from './components/views/about/about'
-import Contact from './components/views/contact/contact'
+// import Contact from './components/views/contact/contact'
 import Footer from './components/footer/footer'
 import './App.css'
 
